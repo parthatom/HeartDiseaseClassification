45fa5547f04fd640f9327844e2277e77fd28bf38
Parth Shah <41008098+parthatom@users.noreply.github.com>
2019-12-26 13:11:56 +0530

    Remove unwanted print statements and add Mean calc

--- gmm_covar.py
+++ gmm_covar.py
@@ -1,35 +1,41 @@
 import torch
 import numpy as np
-
-print('hahah')
-
-print('hello from anyfile notepad.')
-print(' this looks cool, although it is pretty useless')
-
-print(' I also need to figure out if I can directly save both in drive and github ')
-
-print(' Okay lets start ')
-
-print(' firstly i need func that can take X, prob, mean and calculate the covar ')
-print('edited T')
+print("aa")
 
 def gmm_covar(X, means, resps, n_clusters = 2):
     """
-    X shape (n_samples, n_features)
-    resp shape (n_samples, n_clusters)
-    means shape (n_classes, n_features)
-    n_clusters default 2
-    returns Covariance matrix shape (n_features, n_features)
+    
+    Parameters
+    ----------
+    X shape (n_samples, n_features).
+
+    resp shape (n_samples, n_clusters).
+
+    means shape (n_classes, n_features).
+
+    n_clusters (default 2).
+    
+    
+    Returns
+    -------
+    Covariance matrix shape (n_features, n_features);
     """
     n_samples = len(X)
     covar_list = []
     for i in range(n_clusters):
-        resp = resps[:,i].reshape(1, n_samples)
+        resp = resps[:,i]#.reshape(1, n_samples)
         mean = means[i]
         m = np.sum(resp)
-        print(len(resp), m)
-        a = resp * ((X - mean).T) # Element wise multiplication  # Shape (n_samples, n_features)
-        covar = np.dot(a, (X - mean))                       # Shape (n_features, n_features)
+        mean_calculated =  (1/m)*((resp.reshape(n_samples, 1)*X).sum(axis = 0))
+        ratio_means = mean/mean_calculated
+        print(ratio_means)
+        print(ratio_means/X.std(axis = 0))
+        print(ratio_means/X.mean(axis = 0))
+        print(ratio_means.mean(), ratio_means.std())
+        # print(resp.shape, m)
+        print(np.exp(mean), mean_calculated)
+        a = resp * ((X - mean_calculated).T) # Element wise multiplication  # Shape (n_samples, n_features)
+        covar = np.dot(a, (X - mean_calculated))                       # Shape (n_features, n_features)
         covar_list.append(covar)
         covar /= m
     return np.asarray(covar_list)
